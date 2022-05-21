2d56f7192c158498bca87f18c50ad0a7eb253fa5
davimc <davimatosc@hotmail.com>
2022-05-20 22:52:38 -0300

    first commit

---
+++ backend/src/main/java/com/davimc/DSCatalog/DsCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.davimc.DSCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/davimc/DSCatalog/DsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davimc.DSCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
