a1f8b71d82a1f4dae7a4540e4a273b871e417dd4
davimc <davimatosc@hotmail.com>
2022-05-24 08:13:12 -0300

    Seeding data

--- backend/src/main/java/com/davimc/DSCatalog/config/SecurityConfig.java
+++ backend/src/main/java/com/davimc/DSCatalog/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.davimc.DSCatalog.configuration;
+package com.davimc.DSCatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
