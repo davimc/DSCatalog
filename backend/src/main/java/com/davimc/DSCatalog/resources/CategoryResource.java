3978fa932a91b2448b4e78299e4c78dadf8470b6
davimc <davimatosc@hotmail.com>
2022-05-26 03:04:37 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/davimc/DSCatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/davimc/DSCatalog/resources/CategoryResource.java
@@ -1,8 +1,12 @@
 package com.davimc.DSCatalog.resources;
 
+import com.davimc.DSCatalog.DTO.CategoryDTO;
 import com.davimc.DSCatalog.entities.Category;
+import com.davimc.DSCatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +17,14 @@ import java.util.List;
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
+    @Autowired
+    private CategoryService service;
     @GetMapping
-    public ResponseEntity<List<Category>> findAll() {
-        return null;
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        return ResponseEntity.ok().body(service.findAll());
+    }
+    @GetMapping(value="/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
--- backend/src/main/java/com/davimc/DSCatalog/services/CategoryService.java
+++ backend/src/main/java/com/davimc/DSCatalog/services/CategoryService.java
@@ -1,18 +1,33 @@
 package com.davimc.DSCatalog.services;
 
+import com.davimc.DSCatalog.DTO.CategoryDTO;
 import com.davimc.DSCatalog.entities.Category;
 import com.davimc.DSCatalog.repositories.CategoryRepository;
+import com.davimc.DSCatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-    public List<Category> findAll() {
-        return repository.findAll();
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<CategoryDTO> obj = repository.findAll().stream()
+                .map(CategoryDTO::new)
+                .collect(Collectors.toList());
+        return obj;
+    }
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+
+        return new CategoryDTO(obj.orElseThrow(() -> new EntityNotFoundException(id, Category.class)));
     }
 }
