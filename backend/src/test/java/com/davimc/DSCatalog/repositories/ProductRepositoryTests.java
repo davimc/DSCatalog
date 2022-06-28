36f9ac513ce4e13487dee1893d7004a8762b1f11
davimc <davimatosc@hotmail.com>
2022-06-28 01:24:57 -0300

    Test ProductRepository

---
+++ backend/src/test/java/com/davimc/DSCatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,75 @@
+package com.davimc.DSCatalog.repositories;
+
+import com.davimc.DSCatalog.entities.Product;
+import com.davimc.DSCatalog.tests.Factory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository repository;
+    private long existingId;
+    private long nonExistingId;
+    private long countTotalIds;
+
+    @BeforeEach
+    public void setUp() {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalIds = repository.count();
+    }
+
+    @Test
+    public void findByIdShouldReturnNonEmptyOptionalProductWhenIdExist() {
+
+        Assertions.assertDoesNotThrow(()-> {
+            Optional<Product> obj = repository.findById(existingId);
+            Assertions.assertTrue(obj.isPresent());
+            Assertions.assertEquals(obj.get().getId(),existingId);
+        });
+    }
+
+    @Test
+    public void findByIdShouldReturnEmptyOptionalProductWhenIdDoesNotExist() {
+        Assertions.assertDoesNotThrow(()-> {
+            Optional<Product> obj = repository.findById(nonExistingId);
+            Assertions.assertFalse(obj.isPresent());
+        });
+    }
+
+    @Test
+    public void saveShouldCreateAndAutoincrementIdWhenIdIsNull() {
+        Product obj = Factory.createProduct();
+        obj = repository.save(obj);
+
+        Assertions.assertNotNull(obj.getId());
+        Assertions.assertEquals(countTotalIds+1, obj.getId());
+    }
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+
+
+        repository.deleteById(existingId);
+
+        Optional<Product> obj = repository.findById(existingId);
+        Assertions.assertFalse(obj.isPresent());
+    }
+
+    @Test
+    public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+
+
+        Assertions.assertThrows(EmptyResultDataAccessException.class,()->{
+            repository.deleteById(nonExistingId);
+        });
+    }
+}
---
+++ backend/src/test/java/com/davimc/DSCatalog/tests/Factory.java
@@ -0,0 +1,23 @@
+package com.davimc.DSCatalog.tests;
+
+import com.davimc.DSCatalog.DTO.ProductDTO;
+import com.davimc.DSCatalog.entities.Category;
+import com.davimc.DSCatalog.entities.Product;
+
+import java.time.Instant;
+
+public class Factory {
+
+    public static Product createProduct() {
+        Product obj =  new Product("Phone", "Good phone", 2800.75, "./img/img.png", Instant.parse("2020-10-20T03:00:00Z"));
+        obj.getCategories().add(new Category("Eletronics"));
+        return obj;
+    }
+
+    public static ProductDTO createProductDTO() {
+        Product obj = createProduct();
+        ProductDTO dto = new ProductDTO(obj,obj.getCategories());
+
+        return dto;
+    }
+}
