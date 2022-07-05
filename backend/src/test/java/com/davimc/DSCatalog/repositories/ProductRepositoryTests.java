78fad6b6f2ec790db711f9ee7c3e03bbe8c222f3
davimc <davimatosc@hotmail.com>
2022-07-04 23:00:33 -0300

    Finishing ProductRepositoryTests

--- backend/src/test/java/com/davimc/DSCatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/davimc/DSCatalog/repositories/ProductRepositoryTests.java
@@ -1,12 +1,17 @@
 package com.davimc.DSCatalog.repositories;
 
 import com.davimc.DSCatalog.entities.Product;
+import com.davimc.DSCatalog.services.exceptions.DatabaseException;
+import com.davimc.DSCatalog.services.exceptions.ObjectNotFoundException;
 import com.davimc.DSCatalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.Optional;
@@ -18,14 +23,20 @@ public class ProductRepositoryTests {
     private ProductRepository repository;
     private long existingId;
     private long nonExistingId;
+    private long dependentId;
     private long countTotalIds;
 
     @BeforeEach
     public void setUp() {
         existingId = 1L;
         nonExistingId = 1000L;
+        dependentId = 4L;
         countTotalIds = repository.count();
+
+
     }
+
+
 
     @Test
     public void findByIdShouldReturnNonEmptyOptionalProductWhenIdExist() {
