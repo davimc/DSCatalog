fe921fd934e86c491ac3c6b1d0e054e89e979b11
davimc <davimatosc@hotmail.com>
2022-07-31 21:48:15 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/davimc/DSCatalog/services/validation/UserUpdateValidator.java
+++ backend/src/main/java/com/davimc/DSCatalog/services/validation/UserUpdateValidator.java
@@ -1,36 +1,43 @@
 package com.davimc.DSCatalog.services.validation;
 
 import com.davimc.DSCatalog.DTO.UserInsertDTO;
+import com.davimc.DSCatalog.DTO.UserUpdateDTO;
 import com.davimc.DSCatalog.entities.User;
 import com.davimc.DSCatalog.repositories.UserRepository;
 import com.davimc.DSCatalog.resources.exceptions.FieldMessage;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-public class UserUpdateValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
 
+    @Autowired
+    private HttpServletRequest request;
     @Autowired
     private UserRepository repository;
 
     @Override
-    public void initialize(UserInsertValid ann) {
+    public void initialize(UserUpdateValid constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
     }
 
     @Override
-    public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
-
+    public boolean isValid(UserUpdateDTO dto, ConstraintValidatorContext context) {
         List<FieldMessage> list = new ArrayList<>();
 
         // Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
+        var varUri = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        long userId = Long.parseLong(varUri.get("id"));
 
         Optional<User> user = repository.findByEmail(dto.getEmail());
-        if(user.isPresent())
-            if(user.get().getId() != dto.getId())
+        if(user.isPresent() && userId != user.get().getId())
             list.add(new FieldMessage("email", "E-mail já existente"));
 
         for (FieldMessage e : list) {
