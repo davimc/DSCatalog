5c1dc1d4684bf21f26b8314793726d4e1d356e8b
davimc <davimatosc@hotmail.com>
2022-08-09 21:37:13 -0300

    H2 Security configuration

--- backend/src/main/java/com/davimc/DSCatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/davimc/DSCatalog/config/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.davimc.DSCatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,14 +10,18 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
+    private Environment env;
+    @Autowired
     private JwtTokenStore tokenStore;
 
-    private static final String[] PUBLIC = {"/oauth/token"};
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
     private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
     private static final String[] ADMIN = {"/users/**"};
 
@@ -27,6 +32,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        if(Arrays.asList(env.getActiveProfiles()).contains("test"))
+            http.headers().frameOptions().disable();
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
                 .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
